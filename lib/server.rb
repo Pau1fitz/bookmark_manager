fde5bec721944cfe4a9a8a6d3d15a22d92fef3eb
Paul Fitzgerald <paulfitz99@gmail.com>
2015-02-25 18:33:03 +0000

    added password checker

--- lib/server.rb
+++ lib/server.rb
@@ -9,18 +9,15 @@ require './lib/link'
 require './lib/tag'
 require './lib/user'
 
+attr_reader :password
+attr_accessor :password_confirmation
+validates_confirmation_of :password
+
 DataMapper.finalize
 
 DataMapper.auto_upgrade!
 
 class BookmarkManager < Sinatra::Base
-
-  helpers do
-
-    def current_user
-      @current_user ||=User.get(session[:user_id]) if session[:user_id]
-    end
-  end
 
   enable :sessions
   set :session_secret, 'super secret'
@@ -50,11 +47,18 @@ class BookmarkManager < Sinatra::Base
     erb :"users/new"
   end
 
-  post('/users') do
-    User.create(:email => params[:email],
+  post '/users' do
+    user = User.create(:email => params[:email],
                 :password => params[:password])
     session[:user_id] = user.id
     redirect to('/')
+  end
+
+  helpers do
+
+    def current_user
+      @current_user ||=User.get(session[:user_id]) if session[:user_id]
+    end
   end
 
   # start the server if ruby file executed directly
